6163a09cc0991c53a5e09d7c181c7a145cac8d4a
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-02-10 18:10:42 +0800

    sqlupdate docs

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,33 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Construct SQL `SET` clause and parameter values for a partial update query.
+ * 
+ * Given an object containing data to update and a mapping of JavaScript keys to
+ * corresponding SQL column names, this function generates the SQL `SET` clause
+ * and extracts the parameter values for the query.
+ * 
+ * Example:
+ * 
+ *    const dataToUpdate = {
+ *      firstName: 'John',
+ *      age: 30,
+ *    };
+ * 
+ *    const jsToSql = {
+ *      firstName: 'first_name',
+ *      age: 'age',
+ *    };
+ * 
+ *    const { setCols, values } = sqlForPartialUpdate(dataToUpdate, jsToSql);
+ *    // setCols: '"first_name"=$1, "age"=$2'
+ *    // values: ['John', 30]
+ * 
+ * @param {object} dataToUpdate - Object containing data to update.
+ * @param {object} jsToSql - Mapping of JavaScript keys to corresponding SQL column names.
+ * @returns {object} An object containing the SQL `SET` clause and parameter values.
+ * @throws {BadRequestError} If the data to update is empty.
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +35,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
